076b864a4b2cf1aacb0da33dd307e9792c575c84
sdavis43 <57076804+sdavis43@users.noreply.github.com>
2020-02-25 18:10:07 -0600

    all working

--- index.js
+++ index.js
@@ -25,8 +25,6 @@ app.get('/products',(req, res) => {
         const products = await Product
         .find()
         .sort({name: 1})
-        // .count()
-        // console.log(products);
         res.end(`<html><h1>Products</h1>
                 <p>${JSON.stringify(products)}</p>
                 <p>Click <a href="http://localhost:3000/">here</a> to return home.</p>`)
@@ -45,63 +43,78 @@ app.get('/products/:name',(req, res) => {
     findProduct();
 });
 
-app.post('/add_product'),(req,res) => {
-    console.log('adding.....')
-    // async function addProduct() {
-    //     try{
-    //         const product = new Product({
-    //             name: req.body.name,
-    //             quantity: parseInt(req.body.quantity),
-    //             price: parseInt(req.body.price)
-    //         });
-    //         let result = await product.save();
-    //         console.log(result);
-    //         res.end(`<html><h1>Added New Product</h1>
-    //             <p>${JSON.stringify(result)}</p>
-    //             <p>Click <a href="http://localhost:3000/">here</a> to return home.</p>`)
-    //     }
-    //     catch(err){
-    //         console.log('Error', err.message);
-    //     }    
-    // }
-    // addProduct();
-
-    let newProduct = new Product();
-    newProduct.name = req.body.name;
-    newProduct.quantity = req.body.quantity;
-    newProduct.price = req.body.price;
-    newProduct.save((err, product)=>{
-        if(err) {res.send('Error in saving')}
-        else {res.send(product)}
-    })
-}
-
-app.put('/update_product/:name'),(req,res) => {
-    let name = req.params.name;
-    async function updateProduct(name) {
-        const product = await Product.findOneAndUpdate({name: name},{
-            $set:{
+app.post('/add_product',(req, res) => {
+    async function addProduct() {
+        try{
+            const product = new Product({
+                name: req.body.name,
                 quantity: req.body.quantity,
                 price: req.body.price
-            }
-        },{new: true});
-        console.log('update apple',product);
+            });
+            let result = await product.save();
+            // console.log(result);
+            res.end(`<html><h1>Added New Product</h1>
+                <p>${JSON.stringify(result)}</p>
+                <p>Click <a href="http://localhost:3000/">here</a> to return home.</p>`)
+        }
+        catch(err){
+            // console.log('Error', err.message);
+            res.end(`Error: ${err.message}`);
+        }    
+    }
+    addProduct();
+});
+
+app.put('/update_product/:name',(req,res) => {
+    let name = req.params.name;
+    async function updateProduct(name) {
+        try{
+            const product = await Product.findOneAndUpdate({name: name},{
+                $set:{
+                    quantity: req.body.quantity,
+                    price: req.body.price
+                }
+            },{new: true});
+            res.end(`<html><h1>Updated Product</h1>
+                <p>${JSON.stringify(product)}</p>
+                <p>Click <a href="http://localhost:3000/">here</a> to return home.</p>`)
+        }
+        catch(err){
+            // console.log('Error', err.message);
+            res.end(`Error: ${err.message}`);
+        }
     }
     updateProduct(name);
-}
+});
 
-app.delete('/products'),(req,res) => {
-    // delete all products
-}
+app.delete('/delete',(req,res) => {
+    async function deleteAll() {
+        try{
+            const result = await Product.deleteMany();
+            res.end(`Deleted ${JSON.stringify(result)}`);
+        }
+        catch(err){
+            // console.log('Error', err.message);
+            res.send(`Error: ${err.message}`);
+        }
+    }
+    deleteAll();
+});
 
-app.delete('/products/:name'),(req,res) => {
+app.delete('/delete/:name',(req, res) => {
     let name = req.params.name;
     async function deleteProduct(name) {
-        const result = await Product.findOneAndDelete({name: name})
-        console.log('delete apple',result);
+        try{
+            const result = await Product.findOneAndRemove({name: name})
+            res.end(`Deleted ${JSON.stringify(result)}`);
+        }
+        catch(err){
+            // console.log('Error', err.message);
+            res.send(`Error: ${err.message}`);
+        }
     }
     deleteProduct(name);
-}
+});
 
 app.listen(port, () => {
 	console.log('Server listening on port ' + port)
